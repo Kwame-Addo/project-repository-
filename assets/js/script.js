cb83621b1c4fa2f027ae105b86dcd372a5092d21
Kwame Okai Addo <48158461+Kwame-Addo@users.noreply.github.com>
2023-07-16 12:18:54 +0000

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+$("#form").on("submit", (e) => {
+  e.preventDefault();
+  
+  const file_list = $("#file").prop("files");
+  if (_.isEmpty(file_list)) {
+    alert("file Empty");
+    return;
+  }
+  const file = file_list[0];
+  if (file.type.indexOf("image") === -1) {
+    alert("Image");
+    return;
+  }
+
+  let status = {};
+  Tesseract.recognize(file, { lang: $("#lang").val() })
+    .progress((result) => {
+      let p = result.progress * 100;
+      if (!status[result.status]) {
+        status[result.status] = true;
+        $("#progress").append(`
+          <p>${result.status}</p>
+          <div class="progress">
+            <div class="progress-bar progress-bar${_.size(status)}"></div>
+          <div>
+        `);
+      }
+      if (_.isNaN(p)) {
+        p = 100;
+      }
+      $(`.progress-bar${_.size(status)}`)
+        .css({ width: `${p}%` })
+        .text(parseFloat(p).toFixed(2));
+    })
+    .catch(() => {
+      $(".progress-bar").addClass("progress-bar-error");
+      alert("処理に失敗しました");
+    })        
+    .then((result) => {
+      $(".progress-bar").addClass("progress-bar-success");
+      $("#result").text(result.text);
+        // var result = result.text.split(',');
+        var myStr = result.text
+        var newStr = myStr.replace(/\ /g,',');
+        
+
+   
+    alert("OCR Scan complete, please scroll to view result");
+    
+     
+    });
+
+});
+
