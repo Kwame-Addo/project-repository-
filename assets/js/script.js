818abb09b6dd3e36a7d46a1415444611e9a6e32f
Kwame Okai Addo <48158461+Kwame-Addo@users.noreply.github.com>
2023-07-16 21:39:57 +0000

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,19 @@ $("#form").on("submit", (e) => {
 
 });
 
+
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+//margins.left, // x coord   margins.top, { // y coord
+$('#generatePDF').click(function () {
+    doc.fromHTML($('#htmlContent').html(), 15, 15, {
+        'width': 100,
+        'elementHandlers': specialElementHandlers
+    });
+    doc.save('sample_file.pdf');
+});
